6b36d7bb00e7b3dd9e102f22e2c4b250710bbbfb
Tanmay Sinha <alokratn001@gmail.com>
2020-04-15 19:40:15 +0530

    added routes and views

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-const request = require('request');
-const cheerio = require('cheerio');
+const express = require('express');
+const expresslayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
 
-const URL = 'https://clist.by/';
+const app = express();
+const PORT = process.env.PORT || 5000;
 
-request(URL, (error, response, html)=> {
-    if(!error && response.statusCode == 200){
-        const $ = cheerio.load(html);
-    }
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
+
+// EJS
+app.use(expresslayouts);
+app.set('view engine', 'ejs');
+
+// Routes
+app.use('/', require('./routes/home'));
+app.use('/reg', require('./routes/register'));
+
+app.listen(PORT, (err) => {
+    if (!err) console.log("App running on port: ", PORT);
+    else console.log(err);
 });
---
+++ routes/register.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/', (req, res) => {
+
+    const email = req.body.email;
+    if (req.body.sites[0].length == 1 &&
+        req.body.sites[1].length == 1 &&
+        req.body.sites[2].length == 1
+    )
+    {
+        error = 'Please Enter atleast one Site! '
+        res.render('register',{
+            error,
+            email
+        });
+    }
+    else
+    res.send('hello');
+});
+
+module.exports = router;
